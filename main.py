4e6d5d76b34e6046eab579f4bbfe6bdd0f3bfcdd
Heinzle Moritz <121061591+moetzixy@users.noreply.github.com>
2024-01-10 21:30:08 +0100

    GUI

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+import tkinter as tk
+from tkinter import ttk
+
+#window
+window = tk.Tk()
+window.title("Demo")
+window.geometry("300x300")
+
+# title
+title_label = ttk.Label(master = window, text = "Miles to Kilometers", font = "Calibri 24")
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master = window)
+entry = ttk.Entry(master = input_frame, width = 7)
+button = ttk.Button(master = input_frame, text = "Convert")
+# run
+window.mainloop()
